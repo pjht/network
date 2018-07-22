fb9d358eee92def16fa436fdf280836b07994c35
pjht <pjht@users.noreply.github.com>
2018-07-22 13:59:32 -0500

    Initial commit

--- example.rb
+++ example.rb
@@ -1,18 +1,10 @@
 require_relative "netlib.rb"
+require "packetfu"
 cable=EtherCable.new("etherlog.log")
-dev1=EtherSocket.new(cable,[0,0,0,0,0,0])
-dev2=EtherSocket.new(cable,[0,0,0,0,0,1])
-dev3=EtherSocket.new(cable,[0,0,0,0,0,2])
-dev1.register_callback do |packet|
-  p packet
-end
-dev2.register_callback do |packet|
-  p packet
-  dev2.send_packet("hi",packet[0])
-end
-dev3.register_callback do |packet|
-  p packet
-  dev3.send_packet("hi",packet[0])
-end
-dev1.send_packet("hello",[0,0,0,0,0,1])
-dev1.send_packet("hello",[0,0,0,0,0,2])
+dev1=EtherSocket.new(cable,"d2:f5:8a:c4:22:56")
+dev2=EtherSocket.new(cable,"d2:f5:8a:22:65:4c")
+ArpUtils.add_arp_resp(dev2,"192.168.0.56")
+ipsock=MyIPSocket.new(dev1,"192.168.0.34")
+ipsock2=MyIPSocket.new(dev2,"192.168.0.56")
+ipsock.send_packet("hello","192.168.0.56")
+ipsock.send_packet("hi","192.168.0.56")
--- netlib.rb
+++ netlib.rb
@@ -1,20 +1,27 @@
 require "logger"
+require "packetfu"
 class EtherSocket
+  attr_reader :mac
   def initialize(cable,mac)
     @mac=mac
-    @callbacks=[]
+    @callbacks={}
     @cable=cable
     cable.add_device(self)
   end
-  def send_packet(data,to)
-    @cable.send_packet([@mac,to,0,data],self)
+  def send_packet(data,to,type)
+    @cable.send_packet([@mac,to,type,data],self)
   end
-  def register_callback(&block)
-    @callbacks.push block
+  def register_callback(type,&block)
+    if @callbacks[type]==nil
+      @callbacks[type]=[]
+    end
+    @callbacks[type].push block
   end
   def got_packet(packet)
-    return if packet[1]!=@mac
-    @callbacks.each { |cback| cback.call([packet[0],packet[2],packet[3]]) }
+    return if packet[1]!=@mac and packet[1]!="ff:ff:ff:ff:ff:ff"
+    if @callbacks[packet[2]]
+      @callbacks[packet[2]].each { |cback| cback.call([packet[0],packet[3]]) }
+    end
   end
 end
 
@@ -32,18 +39,92 @@ class EtherCable
     @socks.push(socket)
   end
   def send_packet(packet,sock)
-    @logger.info "Frame of type #{packet[2]} from #{mac_to_str(packet[0])} to #{mac_to_str(packet[1])} with data #{packet[3]}" if @log
+    @logger.info "Frame of type #{packet[2]} from #{packet[0]} to #{packet[1]} with data #{packet[3]}" if @log
+    puts "Frame of type #{packet[2]} from #{packet[0]} to #{packet[1]} with data #{packet[3]}" if @log
     @socks.each { |socket| next if sock==socket; socket.got_packet(packet) }
   end
-  private
-  def mac_to_str(mac)
-    old=mac.clone
-    mac=[]
-    old.each do |byte|
-      byte=byte.to_i.to_s(16)
-      byte=byte.rjust(2,"0")
-      mac.push byte
+end
+
+class MyIPSocket
+  def initialize(eth_sock,ip)
+    @sock=eth_sock
+    @ip=ip
+    @resolved={}
+    eth_sock.register_callback(NetLib::ARP) do |packet|
+      packet=ArpUtils::parse_arp_packet(packet)
+      self.got_arp_resp(packet[2],packet[0]) if packet[3]==NetLib::ARP_RESP
     end
-    return "#{mac[0]}:#{mac[1]}:#{mac[2]}:#{mac[3]}:#{mac[4]}:#{mac[5]}"
+    eth_sock.register_callback(NetLib::IP) do |packet|
+      packet=parse_ip_packet(packet)
+      p packet
+    end
+  end
+  def send_packet(msg,to)
+    hdr=PacketFu::IPHeader.new
+    hdr.ip_saddr=@ip
+    hdr.ip_daddr=to
+    hdr.ip_proto=21
+    packet=hdr.to_s+msg
+    if !@resolved[to]
+      ArpUtils.send_arp(@sock,to)
+      blah=0
+      while !@resolved[to]
+        blah=1+1
+      end
+    end
+    @sock.send_packet(packet,@resolved[to],NetLib::IP)
+  end
+  def parse_ip_packet(packet)
+    mac_header=PacketFu::EthHeader.new()
+    mac_header=mac_header.to_s
+    packet=packet[1]
+    packet=mac_header+packet
+    packet=PacketFu::IPPacket.parse packet
+    return packet.payload
+  end
+  def got_arp_resp(ip,mac)
+    @resolved[ip]=mac
+  end
+end
+
+class NetLib
+  ARP=0x0806
+  IP=0x0800
+  ARP_RESP=2
+  ARP_REQ=1
+end
+class ArpUtils
+  def self.add_arp_resp(device,ip)
+    device.register_callback(NetLib::ARP) do |packet|
+      packet=parse_arp_packet(packet)
+      if packet[3]==NetLib::ARP_REQ and packet[1]==ip
+        hdr=PacketFu::ARPHeader.new()
+        hdr.arp_daddr_mac=packet[0]
+        hdr.arp_saddr_mac=device.mac
+        hdr.arp_saddr_ip=packet[1]
+        hdr.arp_daddr_ip=packet[2]
+        hdr.arp_opcode=NetLib::ARP_RESP
+        hdr=hdr.to_s
+        device.send_packet(hdr,packet[0],2054)
+      end
+    end
+  end
+  def self.send_arp(device,ip)
+    hdr=PacketFu::ARPHeader.new()
+    hdr.arp_saddr_mac=device.mac
+    hdr.arp_daddr_ip=ip
+    hdr.arp_opcode=1
+    hdr=hdr.to_s
+    device.send_packet(hdr,"ff:ff:ff:ff:ff:ff",NetLib::ARP)
+  end
+  def self.parse_arp_packet(packet)
+    packet=packet[1]
+    hdr=PacketFu::ARPHeader.new()
+    hdr.read(packet)
+    dst_ip=hdr.arp_daddr_ip
+    src_ip=hdr.arp_saddr_ip
+    src_mac=hdr.arp_src_mac_readable
+    opcode=hdr.arp_opcode
+    return [src_mac,dst_ip,src_ip,opcode]
   end
 end
